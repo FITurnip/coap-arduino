b2a72f217b268d51000d0b6ee48f6adbd0375e98
FITurnip <franklinirtr@gmail.com>
2026-03-04 22:26:23 +0700

    automate setup message

--- coap.h
+++ coap.h
@@ -30,7 +30,7 @@ typedef struct {
   uint8_t  tokenLen;
   uint8_t  code;
   uint16_t messageId;
-  uint8_t *token;
+  uint8_t token[8];
   uint8_t *options;
   uint8_t *payload;
   uint8_t  payloadLen;
@@ -52,8 +52,10 @@ class Coap {
     UDP *_udp;
     int _port;
     int bufferSize;
+    int maxTokenLen;
 
     uint8_t *transmittedMessage = NULL;
+    CoapMessage coapMessage;
     void insertArrayToBuffer(uint16_t &iBuffer, uint8_t *entry, uint16_t entryLen);
     uint16_t setBuffer(CoapMessage &message);
     static bool isAckMessage(const uint8_t* data, int len, uint16_t matchMsgId);
@@ -67,7 +69,11 @@ class Coap {
     Coap(UDP &udp, int port = DEFAULT_COAP_PORT, int bufferSize = DEFAULT_BUFFER_SIZE);
     ~Coap();
     void beginUdp();
-    void transmitMessage(CoapMessage &msg, const char *ip, int port = DEFAULT_COAP_PORT);
+    void initMessage(uint8_t maxTokenLen = 8);
+
+    void setMessage();
+    void transmitMessage(const char *ip, int port = DEFAULT_COAP_PORT);
+
     bool receiveMessage(PacketFilter filter = nullptr, uint16_t matchMsgId = 0);
     void parseReceived(CoapMessage &msg);
     void handleBulkMessage();
--- coap_tx.cpp
+++ coap_tx.cpp
@@ -6,35 +6,35 @@ void Coap::insertArrayToBuffer(uint16_t &iBuffer, uint8_t *entry, uint16_t entry
   }
 }
 
-uint16_t Coap::setBuffer(CoapMessage &msg) {
+uint16_t Coap::setBuffer() {
   if (this->bufferSize < 4) return 0;
 
-  uint8_t tokenLen = msg.tokenLen;
+  uint8_t tokenLen = coapMessage.tokenLen;
   if (tokenLen > 8) return 0;
 
   uint16_t iBuffer = 0;
 
   transmittedMessage[iBuffer++] =
-    (msg.coapVersion << 6) |
-    ((msg.type & 0x03) << 4) |
+    (coapMessage.coapVersion << 6) |
+    ((coapMessage.type & 0x03) << 4) |
     (tokenLen & 0x0F);
 
-  transmittedMessage[iBuffer++] = msg.code;
-  transmittedMessage[iBuffer++] = (msg.messageId >> 8) & 0xFF;
-  transmittedMessage[iBuffer++] = msg.messageId & 0xFF;
+  transmittedMessage[iBuffer++] = coapMessage.code;
+  transmittedMessage[iBuffer++] = (coapMessage.messageId >> 8) & 0xFF;
+  transmittedMessage[iBuffer++] = coapMessage.messageId & 0xFF;
 
-  this->insertArrayToBuffer(iBuffer, msg.token, tokenLen);
+  this->insertArrayToBuffer(iBuffer, coapMessage.token, tokenLen);
 
-  if (msg.payloadLen > 0) {
+  if (coapMessage.payloadLen > 0) {
     if (iBuffer + 1 >= this->bufferSize) return 0;
     transmittedMessage[iBuffer++] = 0xFF;
-    this->insertArrayToBuffer(iBuffer, msg.payload, msg.payloadLen);
+    this->insertArrayToBuffer(iBuffer, coapMessage.payload, coapMessage.payloadLen);
   }
 
   return iBuffer;
 }
 /*
-uint16_t Coap::setBuffer(CoapMessage &msg) {
+uint16_t Coap::setBuffer(CoapMessage &coapMessage) {
   //if (transmittedMessage != NULL) {
     free(transmittedMessage);
     transmittedMessage = NULL;
@@ -42,31 +42,31 @@ uint16_t Coap::setBuffer(CoapMessage &msg) {
   transmittedMessage = (uint8_t *) malloc(this->bufferSize);
   uint16_t iBuffer = 0;
   transmittedMessage[iBuffer++] =
-    (msg.coapVersion << 6) |
-    ((msg.type & 0x03) << 4) |
-    (msg.tokenLen & 0x0F);
+    (coapMessage.coapVersion << 6) |
+    ((coapMessage.type & 0x03) << 4) |
+    (coapMessage.tokenLen & 0x0F);
   
-  //transmittedMessage[0] = msg.coapVersion << 6;
-  //transmittedMessage[0] |= (msg.type & 0x03) << 4;
-  //transmittedMessage[0] |= (msg.tokenLen & 0x0F);
+  //transmittedMessage[0] = coapMessage.coapVersion << 6;
+  //transmittedMessage[0] |= (coapMessage.type & 0x03) << 4;
+  //transmittedMessage[0] |= (coapMessage.tokenLen & 0x0F);
 
-  transmittedMessage[iBuffer++] = msg.code;
-  transmittedMessage[iBuffer++] = (msg.messageId >> 8) & 0xFF;
-  transmittedMessage[iBuffer++] = msg.messageId & 0xFF;
+  transmittedMessage[iBuffer++] = coapMessage.code;
+  transmittedMessage[iBuffer++] = (coapMessage.messageId >> 8) & 0xFF;
+  transmittedMessage[iBuffer++] = coapMessage.messageId & 0xFF;
 
   // here, future for generating token
-  this->insertArrayToBuffer(iBuffer, msg.token, msg.tokenLen);
+  this->insertArrayToBuffer(iBuffer, coapMessage.token, coapMessage.tokenLen);
   // here, future for inserting options
   
-  if (msg.payloadLen > 0) {
+  if (coapMessage.payloadLen > 0) {
     transmittedMessage[iBuffer++] = 0xFF;
-    this->insertArrayToBuffer(iBuffer, msg.payload, msg.payloadLen);
+    this->insertArrayToBuffer(iBuffer, coapMessage.payload, coapMessage.payloadLen);
   }
 
   return iBuffer;
 }*/
 
-void Coap::transmitUdpPacket(CoapMessage &msg, uint16_t bufferLen, const char *ip, int port) {
+void Coap::transmitUdpPacket(CoapMessage &coapMessage, uint16_t bufferLen, const char *ip, int port) {
   yield();
   int ok = this->_udp->beginPacket(ip, port);
   if (!ok) {
@@ -88,20 +88,49 @@ void Coap::transmitUdpPacket(CoapMessage &msg, uint16_t bufferLen, const char *i
   }
   Serial.println();
 }
+test
+void Coap::initMessage(uint8_t maxTokenLen) {
+  this->maxTokenLen = maxTokenLen;
+  if(maxTokenLen > 8) {
+    return;
+  }
+  coapMessage.coapVersion = 1;
+  coapMessage.type = 0;
+  
+  coapMessage.code = 0;
+  coapMessage.messageId = random(0, 65536);
+  coapMessage.payloadLen = 0;
+}
 
-void Coap::transmitMessage(CoapMessage &msg, const char *ip, int port) {
-  uint16_t bufferLen = this->setBuffer(msg);
-  this->transmitUdpPacket(msg, bufferLen, ip, port);
-  /*if(msg.type == 0x00) {
+void Coap::setMessage() {
+  // regenerate token
+  if(maxTokenLen > 8) {
+    return;
+  }
+  coapMessage.tokenLen = random(0, this->maxTokenLen);
+  
+  for(uint8_t i = 0; i < coapMessage.tokenLen; i++) coapMessage.token[i] = random(0, 256);
+  if(coapMessage.tokenLen % 2 == 1) {
+    coapMessage.token[coapMessage.tokenLen - 1] &= 0x07; // LSB
+  }
+
+  // wrap messageId
+  coapMessage.messageId++;
+}
+
+void Coap::transmitMessage(const char *ip, int port) {
+  uint16_t bufferLen = this->setBuffer(coapMessage);
+  this->transmitUdpPacket(coapMessage, bufferLen, ip, port);
+  /*if(coapMessage.type == 0x00) {
     bool isReceivedAck = false;
     int retransmitionCounter = 0;
     int timeout = random(ACK_TIMEOUT, ACK_TIMEOUT * ACK_RANDOM_FACTOR);
     int startListen = millis();
     
     while (!isReceivedAck && millis() < startListen + timeout && retransmitionCounter < MAX_RETRANSMIT) {
-      isReceivedAck = this->receiveMessage(Coap::isAckMessage, msg.messageId);
+      isReceivedAck = this->receiveMessage(Coap::isAckMessage, coapMessage.messageId);
 
-      this->transmitUdpPacket(msg, ip, port);
+      this->transmitUdpPacket(coapMessage, ip, port);
       retransmitionCounter++;
     }
   }*/
