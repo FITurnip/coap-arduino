903538f772b39c1a4a0907c92ce0f6947fa8407e
FITurnip <franklinirtr@gmail.com>
2026-03-06 02:30:09 +0700

    minor

--- src/coap_rx.cpp
+++ src/coap_rx.cpp
@@ -24,22 +24,26 @@ void CoapRx::parseReceived(CoapMessage &msg, uint8_t *buffer, int bufferLen) {
 
   // --- Options (skip) ---
   while (iBuffer < bufferLen && buffer[iBuffer] != 0xFF) {
+    //Serial.printf("%02X\n", buffer[iBuffer]);
     uint8_t optField  = buffer[iBuffer];
     uint16_t optDelta  = buffer[iBuffer] >> 4;
     uint16_t optLen    = buffer[iBuffer] & 0x0F;
 
     if(optDelta == 14) optDelta = buffer[++iBuffer] | buffer[++iBuffer];
     else if(optDelta == 13) optDelta = buffer[++iBuffer];
+    //Serial.printf("%02X\n", buffer[iBuffer]);
 
     if(optLen == 14) optLen = buffer[++iBuffer] | buffer[++iBuffer];
     else if(optLen == 13) optLen = buffer[++iBuffer];
 
+    //Serial.printf("%02X\n", buffer[iBuffer]);
     msg.options[msg.optionSize].num = (msg.optionSize > 0) ? optDelta + msg.options[msg.optionSize - 1].num : optDelta;
     msg.options[msg.optionSize].len = optLen;
-    msg.options[msg.optionSize++].val = (uint8_t*) malloc(optLen);
+    msg.options[msg.optionSize].val = (uint8_t*) malloc(optLen);
     for(uint16_t i = 0; i < optLen; i++) {
       msg.options[msg.optionSize].val[i] = buffer[iBuffer++];
     }
+    msg.optionSize++;
   }
 
   // --- Payload ---
